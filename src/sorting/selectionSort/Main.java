d04dfca6a5cb0f3c8dcc1dc178d2c202edf0dbd0
Yrol Fernando <yrol@Yrols-MacBook-Pro.local>
2024-06-13 20:36:35 +1000

    Selection sort

---
+++ src/sorting/selectionSort/Main.java
@@ -0,0 +1,71 @@
+package sorting.selectionSort;
+
+import java.util.Arrays;
+
+/**
+ * Implementing the selection sort using For and While loop
+ * **/
+public class Main {
+
+    public static int[] selectionSortUsingWhile(int[] arr) {
+
+        int currentPosition = 0;
+        int iterator = 0;
+        int currentSmallestLoc = currentPosition;
+
+        while(currentPosition != arr.length - 1) {
+
+            // Reassigning the current smallest value location if the current iteration value is smaller
+            if(arr[iterator] <  arr[currentSmallestLoc]) {
+                currentSmallestLoc = iterator;
+            }
+
+            if(iterator == arr.length - 1) {
+
+                if(arr[currentPosition] > arr[currentSmallestLoc]) {
+                    int temp = arr[currentPosition];
+                    arr[currentPosition] = arr[currentSmallestLoc];
+                    arr[currentSmallestLoc] = temp;
+                }
+
+                currentPosition++;
+                iterator = currentSmallestLoc = currentPosition;
+            }
+
+            iterator++;
+        }
+
+        return arr;
+    }
+
+    public static int[] selectionSortUsingFor(int[] arr) {
+
+        for(int i = 0; i < arr.length; i++) {
+            int minLoc =  i;
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[minLoc]) {
+                    minLoc = j;
+                }
+            }
+
+            // Reassigning the current smallest value location if the current iteration value is smaller
+            if (arr[i] > arr[minLoc]) {
+                int temp = arr[i];
+                arr[i] = arr[minLoc];
+                arr[minLoc] = temp;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arrWhile = {8,5,2,6,9,3,1,4,0,7};
+        int[] arrFor = {8,5,2,6,9,3,1,4,0,7};
+
+
+        System.out.println(Arrays.toString(selectionSortUsingWhile(arrWhile)));
+        System.out.println(Arrays.toString(selectionSortUsingFor(arrFor)));
+
+    }
+}
